f572f00f160dc8df85c3b8f70e6ff5222e1a0a4a
陈云星 <chen.yack@gmail.com>
2013-04-15 08:50:23 +0000

    Import of CHINAXING/Mail2Wiki-0.001 from CPAN.
    gitpan-cpan-distribution: Mail2Wiki
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         CHINAXING/Mail2Wiki-0.001.tar.gz
    gitpan-cpan-author:       CHINAXING
    gitpan-cpan-maturity:     released

--- lib/Mail2Wiki.pm
+++ lib/Mail2Wiki.pm
@@ -1,179 +1,84 @@
 # ABSTRACT: read mail and send to wiki
 package Mail2Wiki;
-use Mail2Wiki::MailClient;
 use Moose;
+use YAML::XS 'LoadFile';
 use Module::Runtime 'use_module';
-use Config::Tiny;
 use File::Slurp;
-use Log::Any '$log';
+use Mail2Wiki::MailClient;
+use Mail2Wiki::Wiki;
 use Encode ();
 use utf8;
 
 
-
-has 'config_file' => ( is => 'ro', isa => 'Str', default => 'config' );
-
+has 'config_file' => (is => 'ro', isa => 'Str', default => 'config');
 
 has 'config' => (
-    is      => 'ro',
-    isa     => 'Config::Tiny',
-    lazy    => 1,
-    builder => '_build_config',
+  is      => 'ro',
+  isa     => 'HashRef',
+  traits  => ['Hash'],
+  lazy    => 1,
+  builder => '_build_config',
+  handles => {get_config => 'get'},
 );
-
 
 has 'mail_client' => (
-    is      => 'ro',
-    isa     => 'Mail2Wiki::MailClient',
-    lazy    => 1,
-    builder => '_build_mail_client',
+  is      => 'ro',
+  isa     => 'Mail2Wiki::MailClient',
+  lazy    => 1,
+  builder => '_build_mail_client',
 );
 
+has 'wiki' =>
+  (is => 'ro', isa => 'Mail2Wiki::Wiki', lazy => 1, builder => '_build_wiki',);
 
-has 'wiki' => (
-    is      => 'ro',
-    isa     => 'Mail2Wiki::Wiki',
-    lazy    => 1,
-    builder => '_build_wiki',
-);
 
 sub _build_config {
-    my $self = shift;
-    -f $self->config_file
-      or $log->error( "Config file <", $self->config_file, ">unexist : $!" )
-      and exit(1);
-    my $config = Config::Tiny->read( $self->config_file )
-      or $log->error( "read configure file failed: " . Config::Tiny->errstr )
-      and exit(1);
-    return $config;
+  my $self = shift;
+  -f $self->config_file
+    or die "Config file <", $self->config_file, ">unexist : $!";
+  my $conf = LoadFile($self->config_file);
+  return $conf;
 }
 
 sub _build_mail_client {
-    my $self = shift;
-    return Mail2Wiki::MailClient->new( %{ $self->config->{mail} } );
+  my $self = shift;
+  return Mail2Wiki::MailClient->new(
+    server => $self->get_config('mail_server') // "",
+    port   => $self->get_config('mail_port')   // 993,
+    user   => $self->get_config('mail_user')   // "",
+    pass   => $self->get_config('mail_pass')   // "",
+  );
 }
 
 sub _build_wiki {
-    my $self      = shift;
-    my $wiki_type = delete $self->config->{wiki}->{type};
-    return use_module(
-        $wiki_type ? "Mail2Wiki::Wiki::$wiki_type" : "Mail2Wiki::Wiki" )
-      ->new( %{ $self->config->{wiki} } );
+  my $self = shift;
+  my $type = $self->get_config('wiki_type');
+  my $wiki = $type ? "Mail2Wiki::Wiki::$type" : "Mail2Wiki::Wiki";
+  return use_module($wiki)->new(
+    domain => $self->get_config('wiki_domain') // "",
+    user   => $self->get_config('wiki_user')   // "",
+    pass   => $self->get_config('wiki_pass')   // "",
+    prefix => $self->get_config('wiki_prefix') // "",
+  );
 }
-
 
 sub publish {
-    my $self = shift;
+  my $self = shift;
 
-    $log->info('dump new mails from server ...');
+  # Dump Mail from Account
+  $self->mail_client->dump or die "Dump Mail failed";
 
-    # Dump Mail from Account
-    unless ( $self->mail_client->dump ) {
-        $log->error("Dump Mail failed");
-        return;
-    }
-    $log->info('dump Done ...');
-
-    # Post Wiki
-    $log->info('post each mail to wiki ...');
-    while ( my $m = $self->mail_client->get_mail ) {
-        $log->info( " posting : " . Encode::encode( 'utf8', $m->subject ) );
-        eval {
-            $self->wiki->post(
-                subject => $m->subject,
-                file    => $m->file,
-                content => $m->content,
-                poster  => $m->poster,
-            );
-        }
-          or $log->warn( "> post failed of : "
-              . Encode::encode( 'utf8', $m->subject )
-              . ": $@" );
-        $log->info("> Done.");
-    }
-    $log->info('post Done ...');
+  # Post Wiki
+  while (my $m = $self->mail_client->get_mail) {
+    eval {
+      $self->wiki->post(
+        subject => $m->subject,
+        file    => $m->file,
+        content => $m->content,
+        poster  => $m->poster,
+      );
+    } or warn " post failed of : ", Encode::encode('utf8', $m->subject), "\n";
+  }
 }
 
-no Moose;
-__PACKAGE__->meta->make_immutable;
 1;
-
-__END__
-
-=pod
-
-=head1 NAME
-
-Mail2Wiki - read mail and send to wiki
-
-=head1 VERSION
-
-version 0.015
-
-=head1 SYNOPSIS
-
-=head2 Configure
-
-  # config.ini in current dir
-  [mail]
-  server = mail.alibaba-inc.com
-  port   = 993
-  user   = mycount
-  pass   = hello1234
-
-  # Wiki settings
-  [wiki]
-  type   = MindTouch
-  domain = x.wf.taobao.org
-  user   = wikicount
-  pass   = hello1234
-  prefix = PE/mail
-  ;prefix = User
-
-  # create data directory mail2wiki used
-  mkdir data
-
-=head2 Run
-
-  mail2wiki.pl
-
-=encoding utf8
-
-=head1 Attributes
-
-=head2 config_file
-
-configure file, default C<./config>
-
-=head2 config
-
-configure, parsed, an L<Config::Tiny> object, readonly
-
-=head2 mail_client
-
-mail client to dump new mail, a Mail2Wiki::MailClient object, readonly
-
-=head2 wiki
-
-the wiki object to post content to wiki, an abstract object,
-must suplly an instant class use configure : C<wiki_type=foo>, readonly
-
-=head1 method
-
-=head2 publish
-
-B<publish>, which means dump the mail and post to wiki, accomplish all work
-
-=head1 AUTHOR
-
-ChinaXing(陈云星) <chen.yack@gmail.com>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is Copyright (c) 2013 by ChinaXing(陈云星).
-
-This is free software, licensed under:
-
-  The (three-clause) BSD License
-
-=cut
